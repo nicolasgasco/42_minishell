1eab66b5f2899fe2f592fec19c351076e515f8df
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-12 23:47:37 +0100

    Added ft_strdup to prompt text initializer

--- init.c
+++ init.c
@@ -64,17 +64,17 @@ void	ft_init_quotes_data(c_data *c_data)
 	memset(c_data->q_data, 0, sizeof(q_data));
 }
 
-// Structure containing information on prompt
+/* Initializing structure containing information on prompt */
 void	ft_init_prompt_data(c_data *c_data)
 {
 	c_data->p_data = malloc(sizeof(p_data));
 	memset(c_data->p_data, 0, sizeof(p_data));
 	c_data->p_data->username = getenv("USER");
 	if (!c_data->p_data->username)
-		c_data->p_data->username = "username";
+		c_data->p_data->username = ft_strdup("username");
 	c_data->p_data->hostname = getenv("HOSTNAME");
 	if (!c_data->p_data->hostname)
-		c_data->p_data->hostname = "os";
+		c_data->p_data->hostname = ft_strdup("os");
 	c_data->p_data->prompt_text = ft_create_prompt_text(c_data->p_data->username, c_data->p_data->hostname);
-	c_data->p_data->prompt_nl_text = "> ";
+	c_data->p_data->prompt_nl_text = ft_strdup("> ");
 }
