40db5feb5e68e05aa173785a64ceed138bf834f2
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-12 23:48:31 +0100

    Moved prompt data free in correct place

--- free.c
+++ free.c
@@ -3,16 +3,12 @@
 void	ft_free_loop_data(c_data *c_data)
 {
 	ft_free_quotes_data(c_data);
-	ft_free_prompt_data(c_data);
 	ft_deallocate_tokens_list(&c_data->tokens_list);
 }
 
 void	ft_free_common_data(c_data *c_data)
 {
-	if (c_data)
-	{
-		// It is not used at the moment
-	}
+	ft_free_prompt_data(c_data);
 }
 
 void    ft_free_quotes_data(c_data *c_data)
