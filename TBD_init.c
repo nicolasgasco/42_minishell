20f99a09da110c7e3573226b1303d3a851ba02e9
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-12 23:52:22 +0100

    Refactored code

--- TBD_init.c
+++ TBD_init.c
@@ -5,5 +5,6 @@ void    ft_output_epic_welcome(c_data *c_data)
     printf("\n||WAR MACHINE IS READY||\n\n");
     printf("Project Minishell.\n\n");
     printf("Made with love by NICO GASCO & TONI DEL CORRAL.\n\n");
-    printf("Welcome %s, you are now in charge. Good Luck.\n\n", c_data->p_data->username);
+    printf("Welcome %s, you are now in charge. Good Luck.\n", c_data->p_data->username);
+    printf("\n__________________________________________________________________________\n\n");
 }
