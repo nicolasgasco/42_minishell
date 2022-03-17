81183be990acb5f5cc0cb899c903e474a498fbb4
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-17 19:20:48 +0100

    Refactored code

--- expansions_spaces.c
+++ expansions_spaces.c
@@ -59,34 +59,3 @@ void    ft_split_and_generate_node(struct s_qnode *curr)
     new_node->next = curr->next;
     curr->next = new_node;
 }
-
-int ft_calc_token_len(char *str)
-{
-    int i;
-
-    i = 0;
-    while (!ft_isspace(str[i]))
-        i++;
-    return (i);
-}
-
-int ft_has_spaces(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (ft_isspace(str[i]))
-            return (1);
-        i++;
-    }
-    return (0);
-}
-
-int ft_isspace(char c)
-{
-    if (c == '\t' || c == '\r' || c == '\n' || c == '\v' || c == '\f' || c == ' ')
-        return (1);
-    return (0);
-}
