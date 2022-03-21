814a82f8d42c2117970690d24ea97a9e9839ee8f
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 <nicolas.gasco92@gmail.com>
2022-03-21 16:54:08 +0100

    Passing Norminette

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngasco <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 16:52:45 by ngasco            #+#    #+#             */
+/*   Updated: 2022/03/21 16:52:46 by ngasco           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 // static t_sdata sig_data;
@@ -5,7 +17,7 @@
 /* No arguments accepted, global data initialization, loop initialization */
 int	main(int argc, char *argv[], char *envp[])
 {
-	t_cdata t_cdata;
+	t_cdata	t_cdata;
 
 	if (argc != 1 || argv[1])
 	{
@@ -19,7 +31,7 @@ int	main(int argc, char *argv[], char *envp[])
 	return (0);
 }
 
-/* Main loop, input is collected, tokenized, expanded, and sent for processing */
+/* Input is collected, tokenized, expanded, and sent for processing */
 void	ft_start_loop(t_cdata *t_cdata)
 {
 	while (1)
@@ -37,8 +49,8 @@ void	ft_start_loop(t_cdata *t_cdata)
 			ft_output_loop_error_message(t_cdata, "Syntax error");
 			continue ;
 		}
-		ft_expand_spaces(t_cdata);		    
-    	if (!ft_remove_empty_nodes(t_cdata))
+		ft_expand_spaces(t_cdata);
+		if (!ft_remove_empty_nodes(t_cdata))
 		{
 			ft_output_loop_error_message(t_cdata, "Syntax error");
 			continue ;
@@ -47,7 +59,7 @@ void	ft_start_loop(t_cdata *t_cdata)
 		if (!ft_special_chars_are_valid(t_cdata))
 		{
 			ft_output_loop_error_message(t_cdata, "Syntax error");
-			continue;
+			continue ;
 		}
 		ft_create_tokens_list(t_cdata); // Free?
 		// ft_check_cmd(t_cdata);
@@ -56,27 +68,27 @@ void	ft_start_loop(t_cdata *t_cdata)
 	}
 }
 
-void    ft_check_cmd(t_cdata *t_cdata)
+void	ft_check_cmd(t_cdata *t_cdata)
 {
 	t_cdata->cmd = t_cdata->tokens_list->str;
 	ft_print_cmd(t_cdata); // TBD
-    if (ft_strncmp(t_cdata->cmd, "echo", 4) == 0)
-	    built_echo(t_cdata->tokens);
-    else if (ft_strncmp(t_cdata->cmd, "cd", 2) == 0)
-	    built_cd(t_cdata->tokens[1], t_cdata);
-    else if (ft_strncmp(t_cdata->cmd, "pwd", 3) == 0)
-	    built_pwd();
-    else if (ft_strncmp(t_cdata->cmd, "export", 6) == 0)
-	    built_export(t_cdata->tokens + 1, t_cdata);
-    else if (ft_strncmp(t_cdata->cmd, "unset", 5) == 0)
-	    built_unset(t_cdata->tokens + 1, t_cdata);
-    else if (ft_strncmp(t_cdata->cmd, "env", 3) == 0)
-	    built_envp(t_cdata);
-    if (ft_strncmp(t_cdata->cmd, "exit", 4) == 0)
+	if (ft_strncmp(t_cdata->cmd, "echo", 4) == 0)
+		built_echo(t_cdata->tokens);
+	else if (ft_strncmp(t_cdata->cmd, "cd", 2) == 0)
+		built_cd(t_cdata->tokens[1], t_cdata);
+	else if (ft_strncmp(t_cdata->cmd, "pwd", 3) == 0)
+		built_pwd();
+	else if (ft_strncmp(t_cdata->cmd, "export", 6) == 0)
+		built_export(t_cdata->tokens + 1, t_cdata);
+	else if (ft_strncmp(t_cdata->cmd, "unset", 5) == 0)
+		built_unset(t_cdata->tokens + 1, t_cdata);
+	else if (ft_strncmp(t_cdata->cmd, "env", 3) == 0)
+		built_envp(t_cdata);
+	if (ft_strncmp(t_cdata->cmd, "exit", 4) == 0)
 	{
 		ft_free_loop_data(t_cdata);
 		// ft_free_general_data(t_cdata);
-        exit(1);
+		exit(1);
 	}
 	else
 		ft_print_unknown_command();
