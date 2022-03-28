942d5e386a4578e8a323a39c2ab08d6290f80ee9
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-28 17:19:35 +0200

    Fixed issue with special chars expansion

--- expansions_special_chars_1.c
+++ expansions_special_chars_1.c
@@ -1,43 +1,44 @@
 #include "minishell.h"
 
-
-/* Utility function calculating number of chars before special symbol */
-int ft_calc_special_char_token_len(char *str)
+/* Checking if the specified character is in the string */
+int	ft_find_special_char_single(char *str, char c)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] == '>' || str[i] == '<' || str[i] == '|')
+	i = 1;
+    if (str[0] == c && str[1] != c)
+        return (0);
+    while (str[i + 1] != '\0')
+	{
+		if (str[i] == c && str[i + 1] != c && str[i - 1] != c)
             return (i);
-        i++;
-    }
-    return (i);
+		i++;
+	}
+	if (str[i] == c && str[i - 1] != c)
+		return (i);
+	return (-1);
 }
 
 /* Splitting string containing >> and add linked list nodes */
-void    ft_split_special_char_node(struct s_qnode  *curr, int len, char *set)
+void ft_split_special_char_node(struct s_qnode *curr, int len, char *set, int i)
 {
-    char            *token;
-    char            *rest;
-    int             t_len;
-    char            *symbol;
+    char *token;
+    char *rest;
+    char *symbol;
 
-    t_len = ft_calc_special_char_token_len(curr->str);
-    token = (char *)malloc(sizeof(char) * (t_len + 1));   
-    ft_strlcpy(token, curr->str, t_len + 1);
-    rest = ft_substr(curr->str, t_len + len, ft_strlen(curr->str) - t_len - len);
+    token = (char *)malloc(sizeof(char) * (i + 1));
+    ft_strlcpy(token, curr->str, i + 1);
+    rest = ft_substr(curr->str, i + len, (ft_strlen(curr->str) - i - len));
     symbol = (char *)malloc(sizeof(char) * ft_strlen(set) + 1);
     symbol = ft_strdup(set);
     ft_add_special_char_nodes(curr, token, symbol, rest);
 }
 
-/* Adding new nodes with the single parts of the split string containing >> */
-void    ft_add_special_char_nodes(struct s_qnode  *curr, char *curr_str, char *next_str, char *rest)
+/* Adding new nodes with the single parts of the split string containing special char */
+void ft_add_special_char_nodes(struct s_qnode *curr, char *curr_str, char *next_str, char *rest)
 {
-    struct s_qnode  *new_node1;
-    struct s_qnode  *new_node2;
+    struct s_qnode *new_node1;
+    struct s_qnode *new_node2;
 
     new_node1 = (struct s_qnode *)malloc(sizeof(struct s_qnode));
     memset(new_node1, 0, sizeof(struct s_qnode));
@@ -48,10 +49,9 @@ void    ft_add_special_char_nodes(struct s_qnode  *curr, char *curr_str, char *n
     curr->length = ft_strlen(curr_str);
     new_node1->str = next_str;
     new_node1->length = ft_strlen(new_node1->str);
-    new_node1->next = curr->next;
-    curr->next = new_node1;
     new_node2->str = rest;
     new_node2->length = ft_strlen(new_node2->str);
+    new_node2->next = curr->next;
+    curr->next = new_node1;
     new_node1->next = new_node2;
 }
-
