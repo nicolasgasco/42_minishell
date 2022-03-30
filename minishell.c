3942200e1c42f22caa99c88aa6f826d44abd5b53
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-03-30 14:14:55 +0200

    Pruned unused code

--- minishell.c
+++ minishell.c
@@ -67,29 +67,3 @@ void	ft_start_loop(t_cdata *t_cdata)
 		ft_free_loop_data(t_cdata);
 	}
 }
-
-void	ft_check_cmd(t_cdata *t_cdata)
-{
-	t_cdata->cmd = t_cdata->tokens_list->str;
-	ft_print_cmd(t_cdata); // TBD
-	// if (ft_strncmp(t_cdata->cmd, "echo", 4) == 0)
-	// 	built_echo(t_cdata->tokens);
-	// else if (ft_strncmp(t_cdata->cmd, "cd", 2) == 0)
-	// 	built_cd(t_cdata->tokens[1], t_cdata);
-	// else if (ft_strncmp(t_cdata->cmd, "pwd", 3) == 0)
-	// 	built_pwd();
-	// else if (ft_strncmp(t_cdata->cmd, "export", 6) == 0)
-	// 	built_export(t_cdata->tokens + 1, t_cdata);
-	// else if (ft_strncmp(t_cdata->cmd, "unset", 5) == 0)
-	// 	built_unset(t_cdata->tokens + 1, t_cdata);
-	// else if (ft_strncmp(t_cdata->cmd, "env", 3) == 0)
-	// 	built_envp(t_cdata);
-	// if (ft_strncmp(t_cdata->cmd, "exit", 4) == 0)
-	// {
-	// 	ft_free_loop_data(t_cdata);
-	// 	// ft_free_general_data(t_cdata);
-	// 	exit(1);
-	// }
-	// else
-	// 	ft_print_unknown_command();
-}
