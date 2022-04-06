a221d99e58211e88b86a85f03fc4d3d71c99992c
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-04-06 22:58:37 +0200

    Accepting also empty error message

--- errors.c
+++ errors.c
@@ -17,7 +17,8 @@ int	ft_output_loop_error_message(t_cdata *t_cdata, char *message)
 	if (t_cdata->syntax_error == 1)
 	{
 		ft_free_quotes_data(t_cdata);
-		printf("Syntax error: %s\n", message);
+		if (*message)
+			printf("Syntax error: %s\n", message);
 	}
 	return (1);
 }
