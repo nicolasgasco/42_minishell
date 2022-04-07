ed3404a5d0431255557c32a8b6c67f61cb020106
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 <nicolas.gasco92@gmail.com>
2022-04-07 18:54:09 +0200

    Tweaked logic for variables inside here docs

--- expansions_vars_0.c
+++ expansions_vars_0.c
@@ -86,7 +86,8 @@ char	*ft_add_variable_value(char *str, t_cdata *t_cdata)
 			else
 			{
 				while ((str[i] >= 'A' && str[i] <= 'Z')
-					|| (str[i] >= 'a' && str[i] <= 'z'))
+					|| (str[i] >= 'a' && str[i] <= 'z')
+					|| (str[i] >= '0' && str[i] <= '9'))
 					i++;
 			}
 			break ;
@@ -113,7 +114,7 @@ char	*ft_expand_var_value(t_cdata *t_cdata, char *str, int start, int end)
 	else
 	{
 		var_name = ft_get_var_name(str, start + 1, end);
-		var_value = getenv(var_name);
+		var_value = ft_get_env(var_name, t_cdata->envp_export);
 		if (var_value == NULL)
 			result = ft_remove_var_name(str, start + 1, end);
 		else
