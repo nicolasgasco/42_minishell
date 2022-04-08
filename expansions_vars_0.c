164e8b1ce9055aca8fbe661fe7d308ff20be39df
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-04-08 08:00:31 +0200

    Env variables name now accept underscores

--- expansions_vars_0.c
+++ expansions_vars_0.c
@@ -87,7 +87,8 @@ char	*ft_add_variable_value(char *str, t_cdata *t_cdata)
 			{
 				while ((str[i] >= 'A' && str[i] <= 'Z')
 					|| (str[i] >= 'a' && str[i] <= 'z')
-					|| (str[i] >= '0' && str[i] <= '9'))
+					|| (str[i] >= '0' && str[i] <= '9')
+					|| (str[i] == '_'))
 					i++;
 			}
 			break ;
