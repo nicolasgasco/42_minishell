3608ede9e81efea853e10ce7001fbcbada6ac68d
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-04-08 12:45:14 +0200

    Fixed invalid memory error

--- ft_getenv.c
+++ ft_getenv.c
@@ -7,16 +7,17 @@ char	*ft_get_env(char *var_name, char **env)
 	char	*temp2;
 	
 	temp = ft_strjoin(var_name, "=");
-	printf("temp: %s\n", temp);
+	// printf("temp: %s\n", temp);
 	temp2 = NULL;
 	i = 0;
 	while(env[i])
 	{
 		if(!ft_strncmp(env[i], temp, ft_strlen(temp)))
 		{
-			printf("llego\n");
-			temp2 = ft_substr(env[i], ft_strlen(temp) + 1, ((ft_strlen(env[i]) - ft_strlen(temp) - 2)));
-			break ;
+			// printf("llego\n");
+			temp2 = ft_substr(env[i], ft_strlen(temp), ((ft_strlen(env[i]) - ft_strlen(temp))));
+			free(temp);
+			return (temp2);
 		}
 		i++;
 	}
